6123ea25d2c228a2fafdafb78f3a17b4bb6b316e
sagexx <116120242+sagexx@users.noreply.github.com>
2023-02-05 22:11:24 -0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const PORT = process.env.PORT || 3306;
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.static('public'));
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+app.listen(PORT, () => {
+    
+    console.log(`server now live on port ${PORT}!`);
+});
+
+
